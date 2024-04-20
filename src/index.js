2de01691f82fd1c4a8b9e749f2421e37067677b5
RoynevS <109765130+RoynevS@users.noreply.github.com>
2024-04-20 19:49:21 +0200

    Create mobile menu

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 import "./styles/style.css";
 
-console.log("Template Test");
+const openDropDown = (function () {
+  const dropDown = document.querySelector(".drop-down");
+  const body = document.querySelector("body");
+
+  const clickHandler = (event) => {
+    if (event.target.closest(".menu-btn")) {
+      dropDown.classList.toggle("active");
+    } else if (
+      !event.target.closest(".menu-btn") &&
+      !event.target.closest(".drop-down")
+    ) {
+      dropDown.classList.remove("active");
+    }
+  };
+
+  body.addEventListener("click", clickHandler);
+})();
